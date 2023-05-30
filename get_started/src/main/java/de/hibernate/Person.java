22c2cfde60e0d296eca75b2b137564a58ee9af66
ingogutwin <user.name>
2023-05-30 11:57:59 +0200

    Add configuration files and make general configuration for hibernate

--- get_started/src/main/java/de/hibernate/Person.java
+++ get_started/src/main/java/de/hibernate/Person.java
@@ -6,11 +6,14 @@ public class Person {
     private String lastName;
     private int age;
 
+    public Person() {}
+
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
+
 
     public int getId() {
         return id;
