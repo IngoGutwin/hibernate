bf80dbd3f0e116f5dd75d6897b96f1c6560766d3
ingogutwin <user.name>
2023-06-01 09:16:53 +0200

    Refactor imports

--- frontend/src/main/java/de/hibernate/frontend/Main.java
+++ frontend/src/main/java/de/hibernate/frontend/Main.java
@@ -1,7 +1,6 @@
 package de.hibernate.frontend;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -10,7 +9,7 @@ import de.hibernate.database.PersonHandler;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         BorderPane borderPane = new BorderPane();
         Scene scene = new Scene(borderPane, 800, 800);
         stage.setScene(scene);
